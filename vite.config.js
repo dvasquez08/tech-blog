1f547e16651dceff61d9a9f6372a407579f8b8a8
David Vasquez <dvasquez.422@gmail.com>
2025-07-28 01:11:02 -0600

    Removed Tailwind plugin that was cassing crash

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import typography from '@tailwindcss/typography'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), typography()],
+  plugins: [react(), tailwindcss()],
 }) 
