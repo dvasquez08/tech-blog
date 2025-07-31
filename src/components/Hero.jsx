95c2a653eb55840f4dd58724e55d920a5a9415ae
David Vasquez <dvasquez.422@gmail.com>
2025-07-31 13:42:32 -0600

    Fixed the rendering issue of the Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,16 +4,16 @@ import Logo from "../assets/logo-1.png";
 function Hero() {
   return (
     <div
-      className="bg-cover bg-center bg-no-repeat min-h-[60vh] md:h-[80vh] flex items-center justify-center bg-black"
+      className="bg-cover bg-center bg-no-repeat min-h-[60vh] md:h-[80vh] flex items-center justify-center"
       style={{ backgroundImage: `url(${Bg1})` }}
     >
-      <div className="bg-black/70 size-full">
+      <div className="bg-black/50 size-full">
         <div className="container mx-auto text-center flex flex-col items-center justify-center">
-          <img src={Logo} className="h-36" />
-          <h1 className="text-4xl md:text-5xl lg:text-6xl font-sans font-medium mb-6 text-gray-300">
+          <img src={Logo} className="h-36 mt-20" />
+          <h1 className="text-4xl md:text-5xl lg:text-6xl font-sans font-light mb-6 text-white">
             Tech Things HQ
           </h1>
-          <p className="text-xl text-gray-400 max-w-3xl mx-auto mb-12">
+          <p className="text-xl text-blue-100/80 max-w-3xl mx-auto mb-12">
             Your daily byte of the tech world.
           </p>
         </div>
