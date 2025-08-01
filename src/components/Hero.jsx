4c3a4a222db65578f3e3dd13c43629d3a0e48eb7
David Vasquez <dvasquez.422@gmail.com>
2025-07-31 22:06:23 -0600

    Changed font and rounded the image corners

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,13 +4,13 @@ import Logo from "../assets/logo-1.png";
 function Hero() {
   return (
     <div
-      className="bg-cover bg-center bg-no-repeat min-h-[60vh] md:h-[80vh] flex items-center justify-center"
+      className="bg-cover bg-center bg-no-repeat min-h-[60vh] md:h-[80vh] flex items-center justify-center rounded-lg"
       style={{ backgroundImage: `url(${Bg1})` }}
     >
-      <div className="bg-black/50 size-full">
+      <div className="bg-black/50 size-full rounded-lg">
         <div className="container mx-auto text-center flex flex-col items-center justify-center">
           <img src={Logo} className="h-36 mt-20" />
-          <h1 className="text-4xl md:text-5xl lg:text-6xl font-sans font-light mb-6 text-white">
+          <h1 className="text-4xl md:text-5xl lg:text-6xl font-sans font-medium mb-6 text-gray-300">
             Tech Things HQ
           </h1>
           <p className="text-xl text-blue-100/80 max-w-3xl mx-auto mb-12">
